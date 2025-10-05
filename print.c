e6450dee0a17028dc16283608ff81ba8304d46b3
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-10-05 14:38:07 +0530

    Update print.c

--- print.c
+++ print.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int two = 0  , three = 2 , six = 6;
+    int result = (two + three) * six / three;
 
+    printf("Arithmetic Expression : (%d + %d) * %d / %d \n" , two , three , six , three);
+    printf("Result = %d" , result);
+
+    return 0;
+}
